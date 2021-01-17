8f2656652a4ac80de53a56e02d1e82e63e7f749f
Marcelo8173 <marceloandrebio@gmail.com>
2021-01-16 22:48:10 -0300

    implements crud

--- src/main/java/com/projetoinicial/examplecommaven/clientesController/ClientesController.java
+++ src/main/java/com/projetoinicial/examplecommaven/clientesController/ClientesController.java
@@ -3,24 +3,66 @@ package com.projetoinicial.examplecommaven.clientesController;
 import com.projetoinicial.examplecommaven.domain.clientes.Cliente;
 import com.projetoinicial.examplecommaven.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClientesController {
 
-    @PersistenceContext
-    private EntityManager manager;
-
+//    @PersistenceContext
+//    private EntityManager manager;
     //quero uma instancia dessa classe
     @Autowired
-    private ClienteRepository ClienteRepository;
-    @GetMapping("/clientes")
+    private ClienteRepository clienteRepository;
+    @GetMapping
     public List<Cliente> listar(){
-        return ClienteRepository.findAll();
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id")
+    public ResponseEntity buscar(@PathVariable Long id){
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        if(!clienteRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(id);
+        Cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(!clienteRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(id);
+        Cliente = clienteRepository.delete(cliente);
+
+        return ResponseEntity.noContent();
     }
 }
--- src/main/java/com/projetoinicial/examplecommaven/repository/ClienteRepository.java
+++ src/main/java/com/projetoinicial/examplecommaven/repository/ClienteRepository.java
@@ -4,8 +4,11 @@ import com.projetoinicial.examplecommaven.domain.clientes.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //componente do spring
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 
+//    List<Cliente> finfByName(String name);
 }
